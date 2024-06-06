5d6f14fb5c61face21313117a824ff6a3566e305
denism <den83178@gmail.com>
2024-06-06 20:59:50 +0300

    Add project files.

---
+++ 001_GitProba/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _001_GitProba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
