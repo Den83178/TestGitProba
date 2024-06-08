4ca891f5db15b0d2eb86bda368609e7ca43d6806
denism <den83178@gmail.com>
2024-06-08 11:36:33 +0300

    I added a digit query and dispayed it square

--- 001_GitProba/Program.cs
+++ 001_GitProba/Program.cs
@@ -4,6 +4,9 @@
     {
         static void Main(string[] args)
         {
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine ($"{a} * {a} = {a*a}");
+
             Console.WriteLine("Hello, World!");
         }
     }
